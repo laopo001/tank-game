2fec92de139beff70003aeed5eda2ce24c6ff948
laopo001 <353272497@qq.com>
2019-03-01 19:15:55 +0800

    update scene

--- src/scene/tank.ts
+++ src/scene/tank.ts
@@ -5,7 +5,7 @@
  * @author: liaodh
  * @summary: short description for the file
  * -----
- * Last Modified: Friday, March 1st 2019, 1:18:57 am
+ * Last Modified: Friday, March 1st 2019, 4:55:17 pm
  * Modified By: liaodh
  * -----
  * Copyright (c) 2019 jiguang
@@ -24,7 +24,8 @@ let gltf = app.plugins.gltf.createLoader('./assets/models/CompleteTank.gltf');
 
 // let cubeTexture = CubeTexture.loadImage('assets/images/skybox_px.jpg', 'assets/images/skybox_nx.jpg', 'assets/images/skybox_py.jpg', 'assets/images/skybox_ny.jpg',
 //     'assets/images/skybox_pz.jpg', 'assets/images/skybox_nz.jpg');
-
+const red = new StandardMaterial();
+red.diffuseColor = new Color(1, 0, 0);
 
 let light = new Entity('light')
     .addComponent('light', {
@@ -40,7 +41,7 @@ scene.root.addChild(light);
 let plane = new Entity('plane')
     .addComponent('model', {
         type: 'plane',
-        // material: grassMaterial
+        receiveShadow: true
     })
     .addComponent('collision', {
         type: 'box',
@@ -83,20 +84,45 @@ gltf.loadSenceRoot().then(node => {
     entity.addChild(node);
     node.setLocalPosition(0, -1, 0);
     entity.findByTag('model').forEach(x => {
-
+        x.model.instance.meshs.forEach(drawable => {
+            drawable.castShadow = true;
+        });
     });
 
     scene.root.addChild(entity);
 });
 
 document.getElementById('canvas')!.addEventListener('mousedown', (e) => {
-    let from = camera.camera.screenToWorld(e.x, e.y, camera.camera.instance.nearClip);
-    let to = camera.camera.screenToWorld(e.x, e.y, camera.camera.instance.farClip);
-    let result = scene.systems.rigidbody!.physics.raycastFirst(from, to);
-    if (result) {
-        let pickedEntity = result.entity;
-        console.log(pickedEntity.name);
-    }
+    let from = camera.camera.screenToWorld(e.offsetX, e.offsetY, camera.camera.instance.nearClip);
+    let to = camera.camera.screenToWorld(e.offsetX, e.offsetY, camera.camera.instance.farClip);
+    // let result = scene.systems.rigidbody!.physics.raycastFirst(from, to);
+    // if (result) {
+    //     let pickedEntity = result.entity;
+    //     console.log(pickedEntity.name);
+    // }
+    let bullet = new Entity({ tag: ['bullet'] }).addComponent('model', {
+        type: 'box',
+        material: red,
+        options: {
+            halfExtents: new Vec3(0.1, 0.1, 0.1)
+        }
+    }).addComponent('collision', {
+        type: 'box',
+        // debugger: true,
+        halfExtents: new Vec3(0.1, 0.1, 0.1)
+    }).addComponent('rigidbody', {
+        type: 'dynamic',
+        mass: 0.1
+    }).setPosition(from);
+    scene.root.addChild(bullet);
+    to.scale(0.0002);
+    // to.set(to.x, to.y + 9.82, to.z);
+    console.log(to.data);
+    let position = new Vec3(0, 0, 0);
+    bullet.rigidbody.applyForce(new Vec3(0, 9.82, 0), position).then(_ => {
+        bullet.rigidbody.applyImpulse(to, position);
+    })
+
 }, false);
 
 export { scene };
